09348e163a998a7557dedd83a30b2f21aa96d66a
root <mark@getdentalray.com>
2024-11-26 23:13:43 +0800

    migrate core components away from tailwind and into bootstrap
    Signed-off-by: root <mark@getdentalray.com>

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import * as sass from 'sass';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/scss/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
+    build: {
+        minify: true,
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                implementation: sass,
+                sassOptions: {
+                    outputStyle: 'expanded',
+                    quietDeps: true
+                }
+            }
+        }
+    }
 });
