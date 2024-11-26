51e03d62f75d0b6c2c978d159568d4e0d2bd0efd
root <mark@getdentalray.com>
2024-11-27 01:48:29 +0800

    prevent showing of scope->id, instead we focus on the scope->description
    Signed-off-by: root <mark@getdentalray.com>

--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -1,49 +1,47 @@
-<x-app-layout>
+<x-guest-layout>
     <x-slot name="title">
         {{ __('Authorization Request') }}
     </x-slot>
-    <div class="py-4">
-        <div class="container">
-            <div class="card">
-                <div class="card-body">
-                    <!-- Introduction -->
-                    <p class="mb-4"><strong>{{ $client->name }}</strong> is requesting permission to access your
-                        account.</p>
-                    @if (count($scopes) > 0)
-                        <ul class="list-group list-group-flush mb-4">
-                            @foreach ($scopes as $scope)
-                                <li class="list-group-item">
-                                    <b>{{ Str::headline($scope->id) }}</b>: {{ $scope->description }}
-                                </li>
-                            @endforeach
-                        </ul>
-                    @endif
-                    <div class="d-flex justify-content-center gap-3">
-                        <!-- Authorize Button -->
-                        <form method="post" action="{{ route('passport.authorizations.approve') }}">
-                            @csrf
-                            <input type="hidden" name="state" value="{{ $request->state }}">
-                            <input type="hidden" name="client_id" value="{{ $client->getKey() }}">
-                            <input type="hidden" name="auth_token" value="{{ $authToken }}">
-                            <x-primary-button>
-                                Authorize
-                            </x-primary-button>
-                        </form>
 
-                        <!-- Cancel Button -->
-                        <form method="post" action="{{ route('passport.authorizations.deny') }}">
-                            @csrf
-                            @method('DELETE')
-                            <input type="hidden" name="state" value="{{ $request->state }}">
-                            <input type="hidden" name="client_id" value="{{ $client->getKey() }}">
-                            <input type="hidden" name="auth_token" value="{{ $authToken }}">
-                            <x-danger-button>
-                                Cancel
-                            </x-danger-button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <!-- Introduction -->
+
+
+    <h3 class="text-center mt-3 mb-5"><strong>{{ $client->name }}</strong> would like to:</h3>
+    @if (count($scopes) > 0)
+        <ul class="mb-4">
+            @foreach ($scopes as $scope)
+                <li class="my-2">
+                    {!! $scope->description !!}
+                </li>
+            @endforeach
+        </ul>
+    @endif
+    <div class="d-flex justify-content-center gap-3 mb-3">
+        <!-- Authorize Button -->
+        <form method="post" action="{{ route('passport.authorizations.approve') }}">
+            @csrf
+            <input type="hidden" name="state" value="{{ $request->state }}">
+            <input type="hidden" name="client_id" value="{{ $client->getKey() }}">
+            <input type="hidden" name="auth_token" value="{{ $authToken }}">
+            <x-primary-button style="min-width: 10rem;">
+                Authorize
+            </x-primary-button>
+        </form>
+
+        <!-- Cancel Button -->
+        <form method="post" action="{{ route('passport.authorizations.deny') }}">
+            @csrf
+            @method('DELETE')
+            <input type="hidden" name="state" value="{{ $request->state }}">
+            <input type="hidden" name="client_id" value="{{ $client->getKey() }}">
+            <input type="hidden" name="auth_token" value="{{ $authToken }}">
+            <x-danger-button style="min-width: 10rem;">
+                Cancel
+            </x-danger-button>
+        </form>
     </div>
-</x-app-layout>
+
+    <a href="/" class="text-center d-block mb-2">Learn about permissions</a>
+
+    <p class="mb-3">You can disconnect apps anytime in <a href="#">connected apps</a> within your account settings.</p>
+</x-guest-layout>
